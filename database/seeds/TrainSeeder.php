ec257fb9b66a2b1a9619d27959a6c35b76a67b35
fedepaloz <palozzifederico@gmail.com>
2022-09-20 19:04:10 +0200

    id to add

--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -15,17 +15,19 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-       $new_train = new Train();
-       $new_train-> stazione_di_partenza = 'roma';
-       $new_train-> stazione_di_arrivo = 'venezia';
-       $new_train-> orario_di_partenza= '12';
-       $new_train-> orario_di_arrivo = '15';
-       $new_train-> codice_treno = '12345';
-       $new_train-> numero_carrozze = '22';
-       $new_train-> in_orario = 'si';
-       $new_train-> cancellato = 'no'; 
-       $new_train-> save();
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $new_train = new Train();
+            $new_train->stazione_di_partenza = $faker->cityPrefix();
+            $new_train->stazione_di_arrivo = $faker->cityPrefix();
+            $new_train->orario_di_partenza =$faker->time();
+            $new_train->orario_di_arrivo =$faker->time(); 
+            $new_train->codice_treno = $faker->postcode();
+            $new_train->numero_carrozze = $faker->postcode();
+            $new_train->in_orario = $faker->boolean();
+            $new_train->cancellato = $faker->boolean();
+            $new_train->save();
+        }
     }
 }
-
-
