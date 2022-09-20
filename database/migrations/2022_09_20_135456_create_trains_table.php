e0a012c82600a9353fff1a5d8a115d11904d11cd
fedepaloz <palozzifederico@gmail.com>
2022-09-20 18:36:36 +0200

    table added seeder added, faker init

--- database/migrations/2022_09_20_135456_create_trains_table.php
+++ database/migrations/2022_09_20_135456_create_trains_table.php
@@ -15,6 +15,14 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->string('orario_di_partenza');
+            $table->string('orario_di_arrivo');
+            $table->string('codice_treno');
+            $table->string('numero_carrozze');
+            $table->string('in_orario');
+            $table->string('cancellato');
             $table->timestamps();
         });
     }
@@ -29,3 +37,14 @@ class CreateTrainsTable extends Migration
         Schema::dropIfExists('trains');
     }
 }
+
+
+/* Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato */
--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -1,29 +1,31 @@
 <?php
 
-use App\Models\Train;
+use App\Train;
 
 use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds. 
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        
+       $new_train = new Train();
+       $new_train-> stazione_di_partenza = 'roma';
+       $new_train-> stazione_di_arrivo = 'venezia';
+       $new_train-> orario_di_partenza= '12';
+       $new_train-> orario_di_arrivo = '15';
+       $new_train-> codice_treno = '12345';
+       $new_train-> numero_carrozze = '22';
+       $new_train-> in_orario = 'si';
+       $new_train-> cancellato = 'no'; 
+       $new_train-> save();
     }
 }
 
 
-/* Azienda
-Stazione di partenza
-Stazione di arrivo
-Orario di partenza
-Orario di arrivo
-Codice Treno
-Numero Carrozze
-In orario
-Cancellato */
